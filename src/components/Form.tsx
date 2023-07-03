751ca41904c0e960c163bb76e99aee1690acd381
Maximilian <scientificresearch309@gmail.com>
2023-07-03 13:29:18 +0200

    displaying Name in Form

---
+++ src/components/Form.tsx
@@ -0,0 +1,15 @@
+const Form = () => {
+	return (
+		// <div>Form</div>
+		<form action="">
+			<div className="mb-3">
+				<label htmlFor="name" className="form-label">
+					Name
+				</label>
+				<input id="name" type="text" className="form-control" />
+			</div>
+		</form>
+	);
+};
+
+export default Form;
